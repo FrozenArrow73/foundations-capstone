dcfaa9f2f7411bab8a4330636caee3414c3459ef
Derek Wheeler <frozenarrow73@gmail.com>
2023-03-28 08:58:46 -0600

    fixed form bug

--- client/main.js
+++ client/main.js
@@ -1,12 +1,18 @@
-const setBudgetForm = document.getElementById(setBudgetForm)
-let setBudgetInput = document.getElementById(setBudgetInput)
+const setBudgetForm = document.getElementById("setBudgetForm")
+let setBudgetInput = document.getElementById("setBudgetInput")
+let totalBudget = document.getElementById("totalBudget")
+let budgetRemaining = document.getElementById("budgetRemaining")
 
 
 
 function setbudget (event) {
     event.preventDefault()
+    let budgetInput = setBudgetInput.value
+    setBudgetInput.value = ""
+    let body = {
+        input: budgetInput
+    }
 
-    updateBudgetRemaining()
 }
 
 function updateBudgetRemaining() {
